691d06ea286738b4568cbc7000b35b17faa55ebc
MathChewDoor <167593779+MathChewDoor@users.noreply.github.com>
2024-07-14 12:19:40 +0800

    sdrg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// script.js
 document.addEventListener('DOMContentLoaded', function() {
+    // Example JavaScript: Change the background color of the hero section when the page loads
     document.querySelector('.hero').style.backgroundColor = '#f0f0f0';
 });
